70af26b1780bdae76e4e84cb6ec231909e200243
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2008-02-28 13:16:30 +0100

    [doc,generation] documentation

--- lib/orocos/generation/tasks.rb
+++ lib/orocos/generation/tasks.rb
@@ -7,7 +7,9 @@ module Orocos
 	end
 
 	class TaskContext
+	    # The component this task is part of
 	    attr_reader :component
+	    # The task name
 	    attr_reader :name
 
 	    def initialize(component, name, &block)
@@ -24,21 +26,30 @@ module Orocos
 
 	    # Marks this task as being aperiodic (the default). To make it
 	    # periodic, call #period with the required period
-	    def aperiodic
-		@period = nil
-	    end
+	    def aperiodic; @period = nil end
+
+	    # call-seq:
+	    #	period(period_in_seconds) => period_in_seconds
+	    #
+	    # Sets or gets the task period. Call #aperiodic to make it
+	    # aperiodic
+	    dsl_attribute(:period) { |value| Float(value) }
 
 	    # Returns the Orocos scheduler constant name for this task's
-	    # scheduler class. Call #realtime and #non_realtime to change
-	    # the task scheduling class
+	    # scheduler class. Call #realtime and #non_realtime to change the
+	    # task scheduling class
 	    def rtt_scheduler
 		if @realtime then 'ORO_SCHED_RT'
 		else 'ORO_SCHED_OTHER'
 		end
 	    end
+	    # Marks this task as being part of the realtime scheduling class
 	    def realtime; @realtime = true end
+	    # Marks this task as being part of the non-realtime scheduling
+	    # class
 	    def non_realtime; @realtime = false end
 
+	    # Returns the Orocos value for this task's priority
 	    def rtt_priority
 		case @priority
 		when :highest
@@ -50,13 +61,19 @@ module Orocos
 		end
 	    end
 
-	    dsl_attribute :priority do |value|
-		if value != :highest && value != :lowest
-		    Integer(value)
-		else
-		    value
-		end
-	    end
+	    # Make this task as being of the highest priority allowed by the
+	    # underlying OS
+	    def highest_priority; @priority = :highest end
+	    # Make this task as being of the lowest priority allowed by the
+	    # underlying OS
+	    def lowest_priority;  @priority = :lowest end
+	    # call-seq:
+	    #	priority prio => new_prio
+	    #
+	    # Sets the task priority as an integer value. Allowed values are
+	    # OS-specific, and for now the allowed range is unfortunately not
+	    # available from Ruby
+	    dsl_attribute(:priority) { |value| Integer(value) }
 
 	    def event(name)
 	    end
@@ -69,7 +86,15 @@ module Orocos
 		Property.new(name, type)
 	    end
 
+	    # Generate the code files for this task. This builds to classes:
+	    #
+	    # * a #{task.name}Base class in .orocos/tasks/#{task.name}Base.{cpp,hpp}
+	    #   which is the automatically generated part of the task.
+	    # * a #{task.name} class in tasks/#{task.name}.{cpp,hpp} which is
+	    #   the user-provided part of the task. This class is a public
+	    #   subclass of the Base class.
 	    def generate
+		# Make this task be available in templates as 'task'
 		task = self
 	    
 		base_code_cpp = Generation.render_template 'tasks', 'TaskBase.cpp', binding
