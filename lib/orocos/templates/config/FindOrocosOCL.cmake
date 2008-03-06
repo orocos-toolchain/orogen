cd777cb2b767afa0e815ccbbb7b09c94585d56af
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2008-03-06 16:43:02 +0100

    [generation] cleanup the OCL-related module CMake file

---
+++ lib/orocos/templates/config/FindOrocosOCL.cmake
@@ -0,0 +1,35 @@
+# Find parameters to use the Orocos Component Library
+#
+# If OCL_INSTALL is defined, it is the prefix where OCL is installed
+# 
+# This module defines
+# OrocosOCL_FOUND is set to true if OCL is found, in which case the following
+# variables are defined:
+#    OrocosOCL_INCLUDE_DIR
+#    OrocosOCL_LINK_DIR
+#    OrocosOCL_LIBS
+#    OrocosOCL_DEFINES
+#
+# This module requires pkg-config
+
+FIND_PACKAGE( PkgConfig REQUIRED )
+
+IF(OCL_INSTALL)
+SET(ENV{PKG_CONFIG_PATH} "${OCL_INSTALL}/lib/pkgconfig:$ENV{PKG_CONFIG_PATH}")
+ENDIF(OCL_INSTALL)
+
+PKGCONFIG( "orocos-ocl-${OROCOS_TARGET} >= 1.4" OrocosOCL_FOUND OrocosOCL_INCLUDE_DIR OrocosOCL_DEFINES OrocosOCL_LINK_DIR OrocosOCL_LIBS )
+
+IF( OrocosOCL_FOUND )
+    MESSAGE("   Includes in: ${OrocosOCL_INCLUDE_DIR}")
+    MESSAGE("   Libraries in: ${OrocosOCL_LINK_DIR}")
+    MESSAGE("   Libraries: ${OrocosOCL_LIBS}")
+    MESSAGE("   Defines: ${OrocosOCL_DEFINES}")
+ELSE( OrocosOCL_FOUND )
+    IF( OrocosOCL_FIND_REQUIRED )
+	MESSAGE(FATAL_ERROR "The Orocos Component Library has not been found for the ${OROCOS_TARGET} target")
+    ELSEIF(NOT OrocosOCL_FIND_QUIETLY)
+	MESSAGE(STATUS "The Orocos Component Library has not been found for the ${OROCOS_TARGET} target")
+    ENDIF (OrocosOCL_FIND_REQUIRED)
+ENDIF( OrocosOCL_FOUND )
+
