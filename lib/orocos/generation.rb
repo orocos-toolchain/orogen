479316d050a9d53eee74ac7661ddbf9aa3ba3204
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2008-06-23 12:21:08 +0200

    remove useless file

--- lib/orocos/generation.rb
+++ lib/orocos/generation.rb
@@ -3,7 +3,6 @@ require 'orocos/generation/base'
 
 require 'orocos/generation/toolkit'
 require 'orocos/generation/tasks'
-require 'orocos/generation/build'
 
 require 'orocos/generation/component'
 
