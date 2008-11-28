eeeff7bc4980ea1726a73d366cf2eb2b8e62e65f
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2008-11-28 16:07:28 +0100

    test: TC_Generation#test_simple should be generated in wc

--- test/test_generation.rb
+++ test/test_generation.rb
@@ -7,19 +7,27 @@ class TC_GenerationErrorCases < Test::Unit::TestCase
 
     # Test on a very simple component. This usually catches some buildsystem
     # problems that are hidden by the complexity of full-fledged components
-    def test_simple_component(with_corba = false)
+    def test_simple(with_corba = true)
         component = Generation::Component.new
-        component.load(File.join(TEST_DATA_DIR, "simple_component.orogen"))
-        assert_nothing_raised { component.generate } 
+        copy_in_wc File.join(TEST_DATA_DIR, "simple_component.orogen")
+        in_wc do
+            component.load "simple_component.orogen"
+            assert_nothing_raised { component.generate } 
 
-        if with_corba then component.enable_corba
-        else component.disable_corba
+            if with_corba then component.enable_corba
+            else component.disable_corba
+            end
         end
 
+
         compile_wc(component)
+
+        # Force install in prefix
+        in_prefix do
+        end
     end
-    def test_simple_component_corba
-        test_simple_component(true)
+    def test_simple_without_corba
+        test_simple(false)
     end
 end
 
