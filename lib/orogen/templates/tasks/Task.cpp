26ce961e5b8a686dac0fea1e47aee996cda483f9
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2009-04-23 10:31:37 +0200

    reformat the Task.cpp template for easier diff reading
    We put {} in their own line, so that vimdiff and other diff-based
    editors are more useful.

--- lib/orogen/templates/tasks/Task.cpp
+++ lib/orogen/templates/tasks/Task.cpp
@@ -3,19 +3,24 @@
 using namespace <%= task.component.name %>;
 
 <%= task.basename %>::<%= task.basename %>(std::string const& name<%= ", TaskCore::TaskState initial_state" unless task.fixed_initial_state? %>)
-    : <%= task.basename %>Base(name<%= ", initial_state" unless task.fixed_initial_state? %>) {}
+    : <%= task.basename %>Base(name<%= ", initial_state" unless task.fixed_initial_state? %>)
+{
+}
 
 <% task.self_methods.each do |meth| %>
-<%= meth.signature.gsub('(', " #{task.basename}::#{meth.method_name}(") %> {
+<%= meth.signature.gsub('(', " #{task.basename}::#{meth.method_name}(") %>
+{
 }
 <% end %>
 
 <% task.self_commands.each do |cmd| %>
-bool <%= task.basename %>::<%= cmd.work_method_name %><%= cmd.work_signature %> {
+bool <%= task.basename %>::<%= cmd.work_method_name %><%= cmd.work_signature %>
+{
     return true;
 }
 
-bool <%= task.basename %>::<%= cmd.completion_method_name %><%= cmd.completion_signature %> {
+bool <%= task.basename %>::<%= cmd.completion_method_name %><%= cmd.completion_signature %>
+{
     return true;
 }
 <% end %>
@@ -34,15 +39,30 @@ int <%= task.basename %>::getFileDescriptor() const
 // hooks defined by Orocos::RTT. See <%= task.basename %>.hpp for more detailed
 // documentation about them.
 
-// bool <%= task.basename %>::configureHook() { return true; }
-// bool <%= task.basename %>::startHook() { return true; }
+// bool <%= task.basename %>::configureHook()
+// {
+//     return true;
+// }
+// bool <%= task.basename %>::startHook()
+// {
+//     return true;
+// }
 <% if task.event_ports.empty? %>
-// void <%= task.basename %>::updateHook() {}
+// void <%= task.basename %>::updateHook()
+// {
+// }
 <% else %>
 // void <%= task.basename %>::updateHook(std::vector<RTT::PortInterface*> const& updated_ports)
-// {}
+// {
+// }
 <% end %>
-// void <%= task.basename %>::errorHook() {}
-// void <%= task.basename %>::stopHook() {}
-// void <%= task.basename %>::cleanupHook() {}
+// void <%= task.basename %>::errorHook()
+// {
+// }
+// void <%= task.basename %>::stopHook()
+// {
+// }
+// void <%= task.basename %>::cleanupHook()
+// {
+// }
 
