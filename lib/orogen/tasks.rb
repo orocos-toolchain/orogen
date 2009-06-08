orocos-toolchain/orogen

commit 0175bf0afec76dadd133304fe9e84231b7b07071

fix #include stanza when subclassing a task which is already defined in the same module
Until now, the generated stanza was

  tasks/NAME.hpp

The problem with this is that, once installed, the header is unusable
since task context headers are referenced with

  component_name/NAME.hpp

So fix that stanza.

It fixes usage of task contexts that
 - are subclassing a task defined in the same module
 - are being used in a different project (imported with
   #using_task_library)

lib/orogen/tasks.rb
@@ -430,7 +430,7 @@ module Orocos
                     library_name, name = self.name.split("::")
                     "#{library_name.downcase}/#{name}.hpp"
                 else
-                    "tasks/#{basename}.hpp"
+                    "#{component.name.downcase}/#{basename}.hpp"
                 end
             end
 

commit 28b1ad7b8646bb01f4ec1af6afdf6f6a3a60c1fa

<component>/<taskname>.hpp is not a valid reference for the local component
0175bf0afec76dadd133304fe9e84231b7b07071 introduced a build bug, in which the
tasks' .hpp file was not found by the build system. This bug was lost because
the header was found in the install prefix.

We fix this issue by symlinking Task.cpp into .orogen/tasks/ so that they can be
found with <component>/<taskname>.hpp.

lib/orogen/tasks.rb
@@ -707,6 +707,18 @@ module Orocos
                 return if external_definition?
                 used_toolkits
 
+                # Create a symbolic link in .orogen/tasks that points to
+                # tasks/File.hpp. This is needed for proper
+                # inter-module-dependency management, as the task file needs to
+                # be referred to as <project>/<task>.hpp
+                symlink = File.join(AUTOMATIC_AREA_NAME, 'tasks', "#{basename}.hpp")
+                if !File.exists?(symlink)
+                    Dir.chdir(File.dirname(symlink)) do
+                        FileUtils.ln_sf File.join("..", "..", "tasks", "#{basename}.hpp"), "#{basename}.hpp"
+                    end
+                end
+
+
 		# Make this task be available in templates as 'task'
 		task = self
 	    