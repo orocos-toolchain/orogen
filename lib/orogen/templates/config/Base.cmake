a368ee15cbb9e62b02452b2789136332086ea22f
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2009-07-29 10:57:15 +0200

    fix appending -Wall in CMAKE_CXX_FLAGS
    It was using the variable as a list, but it seems that it should be a
    string. It fixes issues when the user adds its own flags.

--- lib/orogen/templates/config/Base.cmake
+++ lib/orogen/templates/config/Base.cmake
@@ -9,7 +9,7 @@ set(OROCOS_TARGET "<%= component.orocos_target %>")
 include(CheckCXXCompilerFlag)
 CHECK_CXX_COMPILER_FLAG("-Wall", CXX_SUPPORTS_WALL)
 if (CXX_SUPPORTS_WALL)
-    set (CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -Wall)
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
 endif (CXX_SUPPORTS_WALL)
 
 # Define the necessary RPath information for all binaries. That can be stripped
