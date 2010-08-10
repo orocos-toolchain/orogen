orocos-toolchain/orogen

commit bd9e1c7ec475d4770abe307ab3b3740464266058

adapt to the new Typelib::Registry#each API
This fixes a major performance bug caused by the use of has_namespace?
in external_task_context. Loading oroGen projects was causing multiple
iterations on all the types of the project registries. In systems where
quite a few oroGen projects are used, this was a memory and CPU hog

lib/orogen/toolkit.rb
@@ -838,7 +838,7 @@ module Orocos
                 # registry.each_type. Note that the registry is minimal, i.e.
                 # contains only our own types plus the types that are needed to
                 # define them, and this is therefore not a problem.
-		registry.each_type(true) do |name, type|
+		registry.each(:with_aliases => true) do |name, type|
                     loop do
                         component.used_toolkits.each do |tk|
                             if type < Typelib::ArrayType 
@@ -974,7 +974,7 @@ module Orocos
                 # practice, we generate C code that we merge back into the
                 # repository
                 generate_all_marshalling_types = false
-                generated_types = registry.enum_for(:each_type).
+                generated_types = registry.each(:with_aliases => true).
                     find_all { |t| !component.imported_type?(t.name) && !t.inlines_code? }
 		toolkit = self
                 catch(:nothing_to_define) do
@@ -1015,7 +1015,7 @@ module Orocos
             # toolkit.
             def self_typenames
 		generated_types = []
-		registry.each_type(true) do |name, type|
+		registry.each(:with_aliases => true) do |name, type|
                     next if component.imported_type?(name)
 		    if !type.inlines_code?
 			generated_types << name
@@ -1028,7 +1028,7 @@ module Orocos
             # toolkit
             def self_types
 		generated_types = []
-		registry.each_type do |type|
+		registry.each do |type|
                     next if component.imported_type?(type.name)
                     generated_types << type
 		end

lib/orogen/component.rb
@@ -295,9 +295,10 @@ module Orocos
                     name << '/'
                 end
 
-                registry.enum_for(:each_type).any? do |type|
-                    type.namespace =~ /^#{Regexp.quote(name)}/
+                registry.each(name, :with_aliases => true) do |type|
+                    return true
                 end
+                return false
             end
 
             # The deployment modes that are required for this generation

commit d39457ab04838da5fb79540843e233ac3257ca6f

fix small error introduced in bd9e1c7ec475d4770abe307ab3b3740464266058

lib/orogen/toolkit.rb
@@ -974,7 +974,7 @@ module Orocos
                 # practice, we generate C code that we merge back into the
                 # repository
                 generate_all_marshalling_types = false
-                generated_types = registry.each(:with_aliases => true).
+                generated_types = registry.each(:with_aliases => false).
                     find_all { |t| !component.imported_type?(t.name) && !t.inlines_code? }
 		toolkit = self
                 catch(:nothing_to_define) do