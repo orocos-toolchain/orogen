0fdc8004eaa388398d7cf297d7dedb32ee25e384
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-08-13 18:59:18 +0200

    corba: fix misuse of updateAny and friends
    We now use the AnyConversion specialization mechanism, even though
    adding one more template does not make a lot of sense ...

--- lib/orogen/templates/typekit/corba/Type.cpp
+++ lib/orogen/templates/typekit/corba/Type.cpp
@@ -3,70 +3,67 @@
 #include "transports/corba/Convertions.hpp"
 #include <rtt/transports/corba/CorbaTemplateProtocol.hpp>
 
-namespace orogen_typekits {
-    struct <%= type.method_name %>CorbaMarshaller
-        : public RTT::corba::CorbaTemplateProtocol< <%= type.cxx_name %> >
-    {
-        <% if type.opaque?  %>
-        typedef <%= intermediate_type.corba_name(true) %> CorbaType;
-        <% else %>
-        typedef <%= type.corba_name(true) %> CorbaType;
-        <% end %>
-        typedef <%= type.cxx_name %>   BaseType;
-
-        virtual CORBA::Any_ptr createAny( RTT::base::DataSourceBase::shared_ptr source) const;
-        virtual bool updateAny(RTT::base::DataSourceBase::shared_ptr target, CORBA::Any& blob) const;
-    };
-}
-
-CORBA::Any* orogen_typekits::<%= type.method_name %>CorbaMarshaller::createAny( RTT::base::DataSourceBase::shared_ptr source) const
+namespace RTT
 {
-    RTT::internal::DataSource<BaseType>* d = RTT::internal::DataSource<BaseType>::narrow( source.get() );
-    if ( d )
+    namespace corba
     {
-        <% if type.inlines_code? || type <= Typelib::EnumType %>
-        CorbaType corba;
-        if (!toCORBA(corba, d->value()))
-            return false;
-        CORBA::Any_ptr ret = new CORBA::Any();
-        *ret <<= corba;
-        <% else %>
-        std::auto_ptr<CorbaType> corba( new CorbaType );
-        if (!toCORBA(*corba, d->value()))
-            return false;
-        CORBA::Any_ptr ret = new CORBA::Any();
-        *ret <<= corba.release();
-        <% end %>
-        return ret;
-    }
-    return 0;
-}
+        template<>
+        struct AnyConversion< <%= type.cxx_name %> >
+        {
+            <% if type.opaque?  %>
+            typedef <%= intermediate_type.corba_name(true) %> CorbaType;
+            <% else %>
+            typedef <%= type.corba_name(true) %> CorbaType;
+            <% end %>
+            typedef <%= type.cxx_name %>   BaseType;
 
-bool orogen_typekits::<%= type.method_name %>CorbaMarshaller::updateAny(RTT::base::DataSourceBase::shared_ptr target, CORBA::Any& any) const
-{
-    RTT::internal::AssignableDataSource<BaseType>::shared_ptr ad = RTT::internal::AssignableDataSource<BaseType>::narrow( target.get() );
-    if ( ad )
-    {
-        <% if type.inlines_code? || type <= Typelib::EnumType %>
-        CorbaType  corba;
-        if (!(any >>= corba))
-            return false;
-        return fromCORBA(ad->set(), corba);
-        <% else %>
-        CorbaType*  corba;
-        if (!(any >>= corba))
-            return false;
-        bool ret = fromCORBA(ad->set(), *corba);
-        return ret;
-        <% end %>
+            static bool update(const CORBA::Any& any, BaseType& tp)
+            {
+                <% if type.inlines_code? || type <= Typelib::EnumType %>
+                CorbaType  corba;
+                if (!(any >>= corba))
+                    return false;
+                return orogen_typekits::fromCORBA(tp, corba);
+                <% else %>
+                CorbaType*  corba;
+                if (!(any >>= corba))
+                    return false;
+                bool ret = orogen_typekits::fromCORBA(tp, *corba);
+                return ret;
+                <% end %>
+            }
+
+            static CORBA::Any_ptr createAny( BaseType const& tp )
+            {
+                std::auto_ptr< CORBA::Any > ret( new CORBA::Any() );
+                if (!updateAny(tp, *ret))
+                    return 0;
+                return ret.release();
+            }
+
+            static bool updateAny( BaseType const& value, CORBA::Any& any )
+            {
+                <% if type.inlines_code? || type <= Typelib::EnumType %>
+                CorbaType corba;
+                if (!orogen_typekits::toCORBA(corba, value))
+                    return false;
+                any <<= corba;
+                <% else %>
+                std::auto_ptr<CorbaType> corba( new CorbaType );
+                if (!orogen_typekits::toCORBA(*corba, value))
+                    return false;
+                any <<= corba.release();
+                <% end %>
+                return true;
+            }
+        };
     }
-    return false;
 }
 
 namespace orogen_typekits {
     RTT::corba::CorbaTypeTransporter*  <%= type.method_name %>_CorbaTransport()
     {
-        return new <%= type.method_name %>CorbaMarshaller();
+        return new RTT::corba::CorbaTemplateProtocol< <%= type.cxx_name %> >();
     }
 }
 
