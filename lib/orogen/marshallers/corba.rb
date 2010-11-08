b2b9c03427b6512eb53d2928f78b8fe14d9274e0
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-11-08 08:25:12 +0100

    make string support using assignation instead of element-by-element copy

--- lib/orogen/marshallers/corba.rb
+++ lib/orogen/marshallers/corba.rb
@@ -152,12 +152,18 @@ module Orocos
 
     ::Typelib::specialize_model '/std/string' do
         def inlines_code?; true end
-        def corba_name; "char" end
+        def corba_name; "char const*" end
         def corba_arg_type; "char const*" end
-        def corba_ref_type; "CORBA::String_member&" end
+        def corba_ref_type; "char const*" end
 
         def inline_toCorba(result, value, indent)
             "#{indent}#{result} = #{value}.c_str();"
+        end
+        def to_corba(typekit, result, indent)
+            result << "#{indent}corba = value.c_str();\n"
+        end
+        def from_corba(typekit, result, indent)
+            result << "#{indent}value = corba;\n"
         end
     end
 
