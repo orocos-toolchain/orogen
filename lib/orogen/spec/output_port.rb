af2ae7a634071c0416ebbd4afc53db4302fb1774
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-01-24 11:00:15 +0100

    add maximum size specification and computation in OutputPort
    This is meant to be used in transports where typelib is used for
    marshalling, as it allows to specify what maximum size each vector
    can have, and thus allows to compute the maximum marshalled size of
    the data samples.

--- lib/orogen/spec/output_port.rb
+++ lib/orogen/spec/output_port.rb
@@ -64,6 +64,156 @@ module Orocos
                 @port_triggers
             end
 
+            def resolve_max_size_path(type, name)
+                resolved_type = task.project.intermediate_type_for(type)
+                resolved_type = name.split('.').inject(resolved_type) do |resolved_type, element|
+                    match_deference = element.match(/(\[\])*$/)
+
+                    if !match_deference.pre_match.empty?
+                        resolved_type = resolved_type[match_deference.pre_match]
+                    end
+
+                    (match_deference[0].length / 2).times do
+                        resolved_type = resolved_type.deference
+                    end
+                    resolved_type
+                end
+
+                if !(resolved_type <= Typelib::ContainerType)
+                    raise ArgumentError, "#{name} resolves to the #{resolved_type.name} type in #{type.name}, which is not a variable size container"
+                end
+                resolved_type
+            end
+
+            ##
+            # :method: max_sizes
+            #
+            # :call-seq:
+            #   max_sizes('name.to.field' => value, 'name.other' => value) => self
+            #   max_sizes(vector_size, 'name.to.field' => value, ...) => self
+            #   max_sizes => current size specification
+            #
+            # Sets the maximum allowed size for the variable-size containers in
+            # +type+. If the type is a compound, the mapping is given as
+            # path.to.field => size. If it is a container, the size of the
+            # container itself is given as first argument, and the sizes for the
+            # contained values as a second map argument.
+            #
+            # For instance, with the types
+            #
+            #   struct A
+            #   {
+            #       std::vector<int> values;
+            #   };
+            #   struct B
+            #   {
+            #       std::vector<A> field;
+            #   };
+            #
+            # Then sizes on a port of type B would be given with
+            #
+            #   port.max_sizes('field' => 10, 'field.values' => 20)
+            #
+            # while the sizes on a port of type std::vector<A> would be given
+            # with
+            #
+            #   port.max_sizes(10, 'values' => 20)
+            #
+            dsl_attribute :max_sizes do |*value|
+                as_hash =
+                    if value.size == 2
+                        value[1].to_hash.merge('' => value[0].to_int)
+                    else
+                        value[0].to_hash
+                    end
+
+                # Validate that all values are integers and all names map to
+                # known types
+                as_hash.each do |name, value|
+                    resolve_max_size_path(type, name)
+                    value.to_int
+                end
+                as_hash
+            end
+
+            def initialize_max_size_sample(path, sample, max_sizes)
+                sample_t = sample.class
+                if !(sample_t <= Typelib::Type) || !sample_t.contains?(Typelib::ContainerType)
+                    return true
+                end
+
+                path = path.dup
+                puts "#{path.join(".")} #{sample}"
+
+                if sample.kind_of?(Typelib::ContainerType)
+                    max_size = max_sizes[path.join(".")]
+                    if !max_size
+                        return false
+                    end
+
+                    if path.empty? then path = ["[]"]
+                    else path[-1] = "#{path[-1]}[]"
+                    end
+                    element = sample_t.deference.new
+                    if !initialize_max_size_sample(path, element, max_sizes)
+                        return false
+                    end
+
+                    max_size.times do
+                        sample << element
+                    end
+                    return true
+
+                elsif sample.kind_of?(Typelib::ArrayType)
+                    if path.empty? then path = ["[]"]
+                    else path[-1] = "#{path[-1]}[]"
+                    end
+                    element = sample_t.deference.new
+                    if !initialize_max_size_sample(path, element, max_sizes)
+                        return false
+                    end
+
+                    sample_t.size.times do |i|
+                        sample[i] = element
+                    end
+                    return true
+
+                elsif sample.kind_of?(Typelib::CompoundType)
+                    path << nil
+                    sample_t.each_field do |field_name, _|
+                        field = sample.raw_get_field(field_name)
+                        path[-1] = field_name
+                        if !initialize_max_size_sample(path, field, max_sizes)
+                            return false
+                        end
+                    end
+                    return true
+                end
+
+                raise InternalError, "unknown case in initialize_max_size_sample for #{path.join(".")} of type #{sample_t.name}"
+            end
+
+            # Returns the maximum size of a marshalled sample coming out of this
+            # port
+            #
+            # If the type on this port contains variable-size containers, this
+            # method will return nil unless the 'max_sizes' map specifies their
+            # size
+            def compute_max_marshalling_size(type, max_sizes)
+                resolved_type = task.project.intermediate_type_for(type)
+
+                sample = resolved_type.new
+
+                path = []
+                if initialize_max_size_sample(path, sample, max_sizes)
+                    sample.to_byte_array.length
+                end
+            end
+
+            def max_marshalling_size
+                compute_max_marshalling_size(type, max_sizes)
+            end
+
             # call-seq:
             #   triggered_on input_port_name, input_port_name, ...
             #
