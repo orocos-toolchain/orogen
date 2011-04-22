bf129251e4fff42fdbe872d3bbf1a07587b09416
Peter Soetens <peter@thesourceworks.com>
2011-04-22 15:15:05 +0200

    typegen: include boost serialization headers for all stl types.
    Since this list is not automatically generated, we need to list
    them all, and not an arbitrary selection.
    
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>

--- lib/orogen/templates/typekit/Types.hpp
+++ lib/orogen/templates/typekit/Types.hpp
@@ -25,6 +25,12 @@
 #include <boost/serialization/serialization.hpp>
 #include <boost/serialization/array.hpp>
 #include <boost/serialization/vector.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/set.hpp>
+#include <boost/serialization/list.hpp>
+#include <boost/serialization/deque.hpp>
+#include <boost/serialization/utility.hpp>
 
 <%= typekit.m_types_code %>
 
