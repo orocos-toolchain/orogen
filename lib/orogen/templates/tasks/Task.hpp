8bc505c2ed12265c7d0d26795df7c76a0312f3ce
Thomas Roehr <thomas.roehr@dfki.de>
2011-12-22 19:43:58 +0100

    Add constructor documentation

--- lib/orogen/templates/tasks/Task.hpp
+++ lib/orogen/templates/tasks/Task.hpp
@@ -6,6 +6,21 @@
 #include "<%= component.name.downcase %>/<%= task.basename %>Base.hpp"
 
 namespace <%= component.name %> {
+
+    /*! \class <%= task.basename %> 
+     * \brief The task context provides and requires services. It uses an ExecutionEngine to perform its functions.
+     * Essential interfaces are operations, data flow ports and properties. These interfaces have been defined using the oroGen specification.
+     * In order to modify the interfaces you should (re)use oroGen and rely on the associated workflow.
+     * <%= task.doc %>
+     * \details
+     * The name of a TaskContext is primarily defined via:
+     \verbatim
+     deployment 'deployment_name'
+         task('custom_task_name','<%= "#{component.name}::#{task.basename}" %>')
+     end
+     \endverbatim
+     *  It can be dynamically adapted when the deployment is called with a prefix argument. 
+     */
     class <%= task.basename %> : public <%= task.basename %>Base
     {
 	friend class <%= task.basename %>Base;
@@ -16,9 +31,21 @@ namespace <%= component.name %> {
     compact.join("\n") %>
 
     public:
+        /** TaskContext constructor for <%= task.basename %>
+         * \param name Name of the task. This name needs to be unique to make it identifiable via nameservices.
+         * \param initial_state The initial TaskState of the TaskContext. Default is Stopped state.
+         */
         <%= task.basename %>(std::string const& name = "<%= task.name %>"<%= ", TaskCore::TaskState initial_state = Stopped" unless task.fixed_initial_state? %>);
+
+        /** TaskContext constructor for <%= task.basename %> 
+         * \param name Name of the task. This name needs to be unique to make it identifiable for nameservices. 
+         * \param engine The RTT Execution engine to be used for this task, which serialises the execution of all commands, programs, state machines and incoming events for a task. 
+         * <%= "\\param initial_state The initial TaskState of the TaskContext. Default is Stopped state." unless task.fixed_initial_state? %>
+         */
         <%= task.basename %>(std::string const& name, RTT::ExecutionEngine* engine<%= ", TaskCore::TaskState initial_state = Stopped" unless task.fixed_initial_state? %>);
 
+        /** Default deconstructor of <%= task.basename %>
+         */
 	~<%= task.basename %>();
 
         /** This hook is called by Orocos when the state machine transitions
@@ -28,11 +55,12 @@ namespace <%= component.name %> {
          *
          * It is meaningful only if the #needs_configuration has been specified
          * in the task context definition with (for example):
-         *
-         *   task_context "TaskName" do
-         *     needs_configuration
-         *     ...
-         *   end
+         \verbatim
+         task_context "TaskName" do
+           needs_configuration
+           ...
+         end
+         \endverbatim
          */
         // bool configureHook();
 
