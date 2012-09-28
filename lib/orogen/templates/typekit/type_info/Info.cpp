ae477b479096f108597cfbe006a875f3a5a19a80
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-09-28 10:18:41 +0200

    replace DataFlowTypeInfo (which was rock-specific) by the new split-up typeinfo API

--- lib/orogen/templates/typekit/type_info/Info.cpp
+++ lib/orogen/templates/typekit/type_info/Info.cpp
@@ -9,7 +9,7 @@
 
 <% base_class =
     if !Orocos::TypekitMarshallers::TypeInfo::Plugin.rtt_scripting?
-        "RTT::types::DataFlowTypeInfo< #{type.cxx_name} >"
+        "RTT::types::PrimitiveTypeInfo< #{type.cxx_name} >, RTT::types::TemplateConnFactory< #{type.cxx_name} >"
     elsif type.full_name == "/std/string"
         "RTT::types::StdStringTypeInfo"
     else
@@ -27,6 +27,22 @@ namespace orogen_typekits {
 
     RTT::types::TypeInfoGenerator* <%= type.method_name(true) %>_TypeInfo()
     { return new <%= type.method_name(true) %>TypeInfo(); }
+
+<%  if !Orocos::TypekitMarshallers::TypeInfo::Plugin.rtt_scripting? %>
+    bool installTypeInfoObject(TypeInfo* ti) {
+        // aquire a shared reference to the this object
+        boost::shared_ptr< TemplateTypeInfo< <%= type.cxx_name %> > mthis =
+            boost::dynamic_pointer_cast<TemplateTypeInfo<<%= type.cxx_name %>> >( this->getSharedPtr() );
+        assert(mthis);
+        // Allow base to install first
+        PrimitiveTypeInfo<<%= type.cxx_name %>>::installTypeInfoObject(ti);
+        // Install the factories for primitive types
+        ti->setPortFactory( mthis );
+
+        return false;
+    }
+<% end %>
+
 }
 
 <%= Generation.render_template('typekit', 'TemplateInstanciation.cpp', binding) %>
--- lib/orogen/templates/typekit/type_info/OpaqueInfo.cpp
+++ lib/orogen/templates/typekit/type_info/OpaqueInfo.cpp
@@ -8,16 +8,9 @@
 
 <% base_class =
     if !Orocos::TypekitMarshallers::TypeInfo::Plugin.rtt_scripting?
-        "RTT::types::DataFlowTypeInfo< #{type.cxx_name} >"
+        "RTT::types::PrimitiveTypeInfo< #{type.cxx_name} >, RTT::types::TemplateConnFactory< #{type.cxx_name} >"
     else
         "RTT::types::TemplateTypeInfo< #{type.cxx_name} >"
-    end
-
-   intermediate_class =
-    if !Orocos::TypekitMarshallers::TypeInfo::Plugin.rtt_scripting?
-        "RTT::types::DataFlowTypeInfo< #{intermediate_type.cxx_name} >"
-    else
-        "RTT::types::TemplateTypeInfo< #{intermediate_type.cxx_name} >"
     end
 %>
 
@@ -25,8 +18,7 @@ namespace orogen_typekits {
     struct <%= type.method_name(true) %>TypeInfo :
 	public <%= base_class %>
     {
-        typedef <%= intermediate_class %> IntermediateTypeInfo;
-        IntermediateTypeInfo* intermediate_type_info;
+        RTT::types::TypeInfo* intermediate_type_info;
 
         typedef <%= type.cxx_name %> T;
 
@@ -34,22 +26,33 @@ namespace orogen_typekits {
             : <%= base_class %>("<%= type.full_name %>")
         {
             RTT::types::TypeInfoRepository::shared_ptr ti = RTT::types::TypeInfoRepository::Instance();
-            intermediate_type_info = dynamic_cast< IntermediateTypeInfo* >(ti->type("<%= intermediate_type.name %>"));
+            intermediate_type_info = ti->type("<%= intermediate_type.name %>");
+            if (!intermediate_type_info)
+                throw std::runtime_error("cannot initialize TypeInfo for opaque <%= type.full_name %> as I cannot find the TypeInfo for the intermediate type <%= intermediate_type.name %>");
         }
 
 <% if Orocos::TypekitMarshallers::TypeInfo::Plugin.rtt_scripting? %>
-        virtual bool composeTypeImpl(const RTT::PropertyBag& source, RTT::internal::AssignableDataSource<T>::reference_t value) const
+        virtual bool composeType(RTT::base::DataSourceBase::shared_ptr source, RTT::base::DataSourceBase::shared_ptr target) const
         {
             <% if needs_copy %>
             <%= intermediate_type.cxx_name %> intermediate;
-            if (!intermediate_type_info->composeTypeImpl(source, intermediate))
+            typedef RTT::internal::ReferenceDataSource< <%= intermediate_type.cxx_name %> > IntermediateSource;
+            IntermediateSource::shared_ptr intermediate_ptr =
+               new IntermediateSource(intermediate);
+            if (!intermediate_type_info->getCompositionFactory()->composeType(source, intermediate_ptr))
                 return false;
             <% else %>
             std::auto_ptr< <%= intermediate_type.cxx_name %> > intermediate(new <%= intermediate_type.cxx_name %>);
-            if (!intermediate_type_info->composeTypeImpl(source, *intermediate))
+            typedef RTT::internal::ReferenceDataSource< <%= intermediate_type.cxx_name %> > IntermediateSource;
+            IntermediateSource::shared_ptr intermediate_ptr =
+               new IntermediateSource(*intermediate);
+            if (!intermediate_type_info->getCompositionFactory()->composeType(source, intermediate_ptr))
                 return false;
             <% end %>
 
+            RTT::internal::AssignableDataSource<T>::shared_ptr value_source =
+                boost::dynamic_pointer_cast< RTT::internal::AssignableDataSource<T> >( target );
+            <%= type.cxx_name %>& value = value_source->set();
             <%= typekit.code_fromIntermediate(intermediate_type, needs_copy, " " * 8) %>
             return true;
         }
@@ -65,10 +68,10 @@ namespace orogen_typekits {
             <%= type.cxx_name %> const& value = (*value_source).get();
             <%= typekit.code_toIntermediate(intermediate_type, needs_copy, " " * 8) %>
 
-            typedef RTT::internal::ValueDataSource< <%= intermediate_type.cxx_name %> > IntermediateSource;
+            typedef RTT::internal::ConstReferenceDataSource< <%= intermediate_type.cxx_name %> > IntermediateSource;
             IntermediateSource::shared_ptr intermediate_ptr =
                new IntermediateSource(intermediate);
-            return intermediate_type_info->getMember(intermediate_ptr, name);
+            return intermediate_type_info->getMemberFactory()->getMember(intermediate_ptr, name);
         }
 
         virtual RTT::base::DataSourceBase::shared_ptr getMember(RTT::base::DataSourceBase::shared_ptr item,
@@ -83,11 +86,26 @@ namespace orogen_typekits {
 
             <%= type.cxx_name %> const& value = (*value_source).get();
             <%= typekit.code_toIntermediate(intermediate_type, needs_copy, " " * 8) %>
-            typedef RTT::internal::ValueDataSource< <%= intermediate_type.cxx_name %> > IntermediateSource;
+            typedef RTT::internal::ConstReferenceDataSource< <%= intermediate_type.cxx_name %> > IntermediateSource;
             IntermediateSource::shared_ptr intermediate_ptr =
                new IntermediateSource(intermediate);
-            return intermediate_type_info->getMember(intermediate_ptr, id);
+            return intermediate_type_info->getMemberFactory()->getMember(intermediate_ptr, id);
         }
+<% end %>
+
+<%  if !Orocos::TypekitMarshallers::TypeInfo::Plugin.rtt_scripting? %>
+    bool installTypeInfoObject(TypeInfo* ti) {
+        // aquire a shared reference to the this object
+        boost::shared_ptr< TemplateTypeInfo< <%= type.cxx_name %> > mthis =
+            boost::dynamic_pointer_cast<TemplateTypeInfo<<%= type.cxx_name %>> >( this->getSharedPtr() );
+        assert(mthis);
+        // Allow base to install first
+        PrimitiveTypeInfo<<%= type.cxx_name %>>::installTypeInfoObject(ti);
+        // Install the factories for primitive types
+        ti->setPortFactory( mthis );
+        // don't delete us, we're memory managed
+        return false;
+    }
 <% end %>
     };
 
