2ef7ab6d95c00d5bfeb160cb5a9f18fce949978d
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-08-24 19:53:24 +0200

    gen: fix some corner-cases in forward declaration generation
     - don't generate forward declarations for opaques that are
       actually typedefs. It collides with the actual type definition
     - when getting an array type, generate the forward declaration for
       the element type, not the array

--- lib/orogen/templates/typekit/OpaqueFwd.hpp
+++ lib/orogen/templates/typekit/OpaqueFwd.hpp
@@ -3,26 +3,31 @@
 
 <% opaque_types             = type_sets.opaque_types.find_all { |op| !op.generate_templates? } %>
 <% types_containing_opaques = type_sets.types.find_all { |t| t.contains_opaques? && !t.opaque? } %>
-<% all_types = opaque_types.map { |op| op.type } + types_containing_opaques %>
-<% all_types.dup.each do |type|
+<% all_types = opaque_types.map { |op| op.type } + types_containing_opaques
+   all_types.dup.each do |type|
        all_types << typekit.intermediate_type_for(type)
    end
-   templates, classes = all_types.partition do |type|
-       type.name =~ /</
+   all_types.map! do |type|
+       if type <= Typelib::ArrayType
+           while type.respond_to?(:deference)
+               type = type.deference
+           end
+       end
+       type
+   end
+   need_full_definition, forward_declared = all_types.partition do |type|
+       type.name =~ /</ || (type.opaque? && type.metadata.get('opaque_is_typedef').include?('1'))
    end
 %>
 
-<%= template_includes = templates.inject(Set.new) do |all, type|
+<%= includes = need_full_definition.inject(Set.new) do |all, type|
        all |= typekit.include_for_type(type).to_set
     end
-    typekit.cxx_gen_includes(*template_includes)
+    typekit.cxx_gen_includes(*includes)
 %>
 
 <% current_namespace = '/' %>
-<% classes.sort_by(&:name).each do |type| %>
-<% while type.respond_to?(:deference) %>
-<%     type = type.deference %>
-<% end %>
+<% forward_declared.sort_by(&:name).each do |type| %>
 <%= Orocos::Generation.adapt_namespace(current_namespace, type.namespace) %>
 <%     current_namespace = type.namespace %>
 class <%= type.basename %>;
