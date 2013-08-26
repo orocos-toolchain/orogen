e6bee0bd4de2d35a3fd17870a626426675161c6d
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-08-26 07:15:04 +0200

    gen: ORO_CREATE_COMPONENT_* is now part of RTT proper, always enable it

--- lib/orogen/templates/tasks/DeployerComponent.cpp
+++ lib/orogen/templates/tasks/DeployerComponent.cpp
@@ -1,10 +1,12 @@
-#include <ocl/Component.hpp>
-<% component.self_tasks.each do |task| %>
+#include <rtt/Component.hpp>
+
+<% deployable_tasks = component.self_tasks.find_all { |t| !t.abstract? } %>
+<% deployable_tasks.each do |task| %>
 #include <<%= component.name.downcase %>/<%= task.basename %>.hpp>
 <% end %>
 
 ORO_CREATE_COMPONENT_TYPE();
-<% component.self_tasks.each do |task| %>
+<% deployable_tasks.each do |task| %>
 ORO_LIST_COMPONENT_TYPE( <%= task.name %> );
 <% end %>
 
