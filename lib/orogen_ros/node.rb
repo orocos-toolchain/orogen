fef5a8bfc7a8e6cd2cf751de2c7dd3c51ae38a1a
Thomas Roehr <thomas.roehr@dfki.de>
2013-10-17 19:31:53 +0200

    Cleanup Node

--- lib/orogen_ros/node.rb
+++ lib/orogen_ros/node.rb
@@ -2,8 +2,7 @@ module Orocos::ROS
     module Spec
         # Representation of a ROS node as an oroGen model
         class Node < Orocos::Spec::TaskContext
-            # @return [String] the actual name on the ROS side
-            #   {name} returns the node-local name (the "port" name)
+            # @return [String] the name on the ROS side
             attr_accessor :ros_name
 
             # @return [String] the actual name of the package this node is
@@ -20,7 +19,6 @@ module Orocos::ROS
             # @return [OutputTopic]
             def output_topic(topic_name, name, message_type)
                 data_type = ::Orocos::ROS.map_message_type_to_orogen(message_type)
-                puts "DATA_TYPE: #{data_type}"
                 topic = output_port(name, data_type, :class => OutputTopic)
                 topic.ros_name = topic_name
                 topic
@@ -39,6 +37,10 @@ module Orocos::ROS
                 topic = input_port(name, data_type, :class => InputTopic)
                 topic.ros_name = topic_name
                 topic
+            end
+
+            def to_s
+                "#{self.class} ros_name: #{ros_name}, ros_package: #{ros_package}"
             end
         end
     end
