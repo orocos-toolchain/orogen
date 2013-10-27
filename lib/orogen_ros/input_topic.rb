ecce39986c78695c07c1f72a8ea0fb16fc54a2f6
Thomas Roehr <thomas.roehr@dfki.de>
2013-10-27 13:10:19 +0100

    Alias ros_name for output and input topic with :topic_name

--- lib/orogen_ros/input_topic.rb
+++ lib/orogen_ros/input_topic.rb
@@ -4,6 +4,8 @@ module Orocos::ROS
             # @return [String] the actual name on the ROS side
             #   {name} returns the node-local name (the "port" name)
             attr_accessor :ros_name
+
+            alias :topic_name :ros_name
         end
     end
 end
