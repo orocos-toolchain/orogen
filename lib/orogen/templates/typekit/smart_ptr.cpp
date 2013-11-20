c01ff32b4f02b3f675b7eaee553ff8070a9a6562
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-11-20 10:40:37 +0100

    in smart pointer support, throw if toIntermediate is called on an unset smart pointer

--- lib/orogen/templates/typekit/smart_ptr.cpp
+++ lib/orogen/templates/typekit/smart_ptr.cpp
@@ -2,6 +2,8 @@
 
 <%= into.arg_type %> orogen_typekits::toIntermediate(<%= from.arg_type %> real_type)
 {
+    if (!real_type.get())
+        throw std::runtime_error("trying access the value inside an unset smart pointer");
     return *real_type;
 }
 bool orogen_typekits::fromIntermediate(<%= from.ref_type %> real_type, <%= into.cxx_name %>* intermediate)
