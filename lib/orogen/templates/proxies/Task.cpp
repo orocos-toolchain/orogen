e262b1a3e53ede5cef5e5ad3b4f55ead1969e279
Janosch Machowinski <janosch.machowinski@dfki.de>
2015-03-24 09:09:29 +0100

    changed 'component' to 'project'

--- lib/orogen/templates/proxies/Task.cpp
+++ lib/orogen/templates/proxies/Task.cpp
@@ -2,32 +2,32 @@
 
 
 #include <rtt/typekit/RealTimeTypekit.hpp>
-<% if component.enabled_transports.include?('corba') %>
+<% if project.enabled_transports.include?('corba') %>
 #include <rtt/transports/corba/TransportPlugin.hpp>
 <% end %>
-<% if component.enabled_transports.include?('mqueue') %>
+<% if project.enabled_transports.include?('mqueue') %>
 #include <rtt/transports/mqueue/TransportPlugin.hpp>
 <% end %>
 
-<% if component.typekit || !component.used_typekits.empty? %>
+<% if project.typekit || !project.used_typekits.empty? %>
 #include <rtt/types/TypekitPlugin.hpp>
 <% end %>
-<% if typekit = component.typekit %>
+<% if typekit = project.typekit %>
 #include <<%= typekit.name %>/typekit/Plugin.hpp>
-<% component.enabled_transports.each do |transport_name| %>
+<% project.enabled_transports.each do |transport_name| %>
 #include "typekit/transports/<%= transport_name %>/TransportPlugin.hpp"
 <% end %>
 <% end %>
-<% component.used_typekits.each do |tk| %>
+<% project.used_typekits.each do |tk| %>
     <% next if tk.virtual? %>
     <% next if tk.name == "logger" %>
 #include <<%= tk.name %>/typekit/Plugin.hpp>
-    <% component.enabled_transports.each do |transport_name| %>
+    <% project.enabled_transports.each do |transport_name| %>
 #include <<%= tk.name %>/transports/<%= transport_name %>/TransportPlugin.hpp>
     <% end %>
 <% end %>
 
-namespace <%= component.name %> {
+namespace <%= project.name %> {
 
 namespace proxies {
 
@@ -42,25 +42,25 @@ namespace proxies {
 void <%= task.basename %>Initializer::initTypes()
 {
     RTT::types::TypekitRepository::Import( new RTT::types::RealTimeTypekitPlugin );
-    <% if component.enabled_transports.include?('corba') %>
+    <% if project.enabled_transports.include?('corba') %>
     RTT::types::TypekitRepository::Import( new RTT::corba::CorbaLibPlugin );
     <% end %>
-    <% if component.enabled_transports.include?('mqueue') %>
+    <% if project.enabled_transports.include?('mqueue') %>
     RTT::types::TypekitRepository::Import( new RTT::mqueue::MQLibPlugin );
     <% end %>
 
-    <% if component.typekit %>
-    RTT::types::TypekitRepository::Import( new orogen_typekits::<%= component.name %>TypekitPlugin );
-        <% component.enabled_transports.each do |transport_name| %>
+    <% if project.typekit  %>
+    RTT::types::TypekitRepository::Import( new orogen_typekits::<%= project.name %>TypekitPlugin );
+        <% project.enabled_transports.each do |transport_name| %>
     RTT::types::TypekitRepository::Import( new <%= typekit.transport_plugin_name(transport_name) %> );
         <% end %>
     <% end %>
-    <% component.used_typekits.each do |tk| %>
+    <% project.used_typekits.each do |tk| %>
         <% next if tk.virtual? %>
         <% next if tk.name == "logger" %>
 
    RTT::types::TypekitRepository::Import( new orogen_typekits::<%= tk.name %>TypekitPlugin );
-       <% component.enabled_transports.each do |transport_name| %>
+       <% project.enabled_transports.each do |transport_name| %>
    RTT::types::TypekitRepository::Import( new <%= Orocos::Generation::Typekit.transport_plugin_name(transport_name, tk.name) %> );
        <% end %>
    <% end %>
