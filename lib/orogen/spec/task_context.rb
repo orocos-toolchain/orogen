0deed55a56f02924cc88b8473df617f8b93ba027
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2015-04-29 10:17:20 +0200

    Fixed deprecated backward compatibility for base_body argument

--- lib/orogen/spec/task_context.rb
+++ lib/orogen/spec/task_context.rb
@@ -770,7 +770,7 @@ module OroGen
                 if body
                     OroGen.warn "body argument for hidden_operation '#{self.name}'.'#{name}' is deprecated, please set the body during generation phase"
                     OroGen.warn "If you call this from a plugin, define the implementation within your :early_register_for_generation method"
-                    op.base_body = body
+                    op.base_body(body)
                 end
                 op
             end
