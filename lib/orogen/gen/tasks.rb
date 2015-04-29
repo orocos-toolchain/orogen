e96f2092c6a1e563a3f717c097e143679102d2b6
Janosch Machowinski <janosch.machowinski@dfki.de>
2015-04-29 13:19:07 +0200

    Improved error message in case that operation has no body

--- lib/orogen/gen/tasks.rb
+++ lib/orogen/gen/tasks.rb
@@ -243,7 +243,7 @@ EOF
                 end
 
                 if hidden? && !self.base_body
-                    raise InternalError, "a hidden operation must have a body"
+                    raise InternalError, "a hidden operation #{name} must have a body"
                 end
 
 
